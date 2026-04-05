4f00b8d9f840344289aff6de10ca400d61f970b2
Shruti Jain <shrutijain1221@gmail.com>
2026-04-05 16:19:40 +0530

    fix wildcard route

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,11 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"./frontend/dist")))
-    app.get("/*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"./frontend","dist","index.html"))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend", "dist")))
+
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
     })
 }
 server.listen(PORT,()=>{
