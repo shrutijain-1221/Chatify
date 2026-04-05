cebe336a7057beb17d3031caa4a2d105d350cd22
Shruti Jain <shrutijain1221@gmail.com>
2026-04-05 16:26:16 +0530

    fix dist path correctly

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,10 @@ app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend", "dist")))
+    app.use(express.static(path.join(__dirname, "../frontend", "dist")))
 
     app.use((req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
 server.listen(PORT,()=>{
